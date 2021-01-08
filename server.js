25d6d61b002abb41c839b365bcafb716ce0e9047
Ishan <ishanbagchi4@gmail.com>
2021-01-08 23:08:36 +0530

    finish react

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
+const cors = require('cors')
 const schema = require('./schema')
+const path = require('path')
 
 const app = express()
+
+// Allow cors-origin
+app.use(cors())
 
 app.use('/graphql', graphqlHTTP({
     schema,
@@ -10,7 +15,12 @@ app.use('/graphql', graphqlHTTP({
 }),
 )
 
-const PORT = process.env.PORT || 3000
+app.use(express.static('public'))
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+})
+
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
     console.log(`Server started on port: ${PORT}`)
